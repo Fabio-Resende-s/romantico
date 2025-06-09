7357c743cc8c89956ee0b24bfbd43c99c172f0f0
fabio <fabioresende.am@gmail.com>
2025-06-09 18:50:37 -0400

    feliz dia dos namo colocação de fotos

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function revelarMensagem() {
   const msg = document.getElementById("mensagemSecreta");
   msg.style.display = "block";
 }
+function revelarMensagem() {
+  const msg = document.getElementById("mensagemSecreta");
+  const foto = document.getElementById("fotoSurpresa");
+  
+  msg.style.display = "block";
+  foto.style.display = "block";
+}
