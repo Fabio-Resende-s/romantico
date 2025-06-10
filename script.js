52fbb3dc319fea1057c29c5fe3319dc40fa8ced5
fabio <fabioresende.am@gmail.com>
2025-06-10 00:23:35 -0400

    nova atualização

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ function revelarMensagem() {
 function revelarMensagem() {
   const msg = document.getElementById("mensagemSecreta");
   const foto = document.getElementById("fotoSurpresa");
-  
+
   msg.style.display = "block";
   foto.style.display = "block";
 }
+
+ let slideAtual = 0;
+    const slides = document.querySelectorAll('.slide');
+
+    function mostrarSlide(index) {
+      slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+      });
+    }
+
+    function mudarSlide(direcao) {
+      slideAtual += direcao;
+      if (slideAtual < 0) slideAtual = slides.length - 1;
+      if (slideAtual >= slides.length) slideAtual = 0;
+      mostrarSlide(slideAtual);
+    }
